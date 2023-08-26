7f947c05a049c52bc7460f17f56cb351a8b6a645
Matvey <mtvy44@gmail.com>
2023-08-26 06:04:10 +0300

    Added HTML to png functionality with the data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,6 +7,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
+import tkinter as tk
+from tkinter import filedialog
+
+
+red_text = "\033[31mRed Text\033[0m"
+green_text = "\033[32mGreen Text\033[0m"
+yellow_text = "\033[33mYellow Text\033[0m"
 
 def find_element(single_element,name):
     try:
@@ -16,9 +26,9 @@ def find_element(single_element,name):
             array.append(temp)
         return array
     except Exception as e:
-        print("couldn't find a element. returning empty array")
         print("An error occurred at finding element:", e)
-        return []
+        raise ValueError("couldn't find a element. Relaunch the exe")
+        
 
 def find_element_tag(single_element,name,tag):
     try:
@@ -30,9 +40,18 @@ def find_element_tag(single_element,name,tag):
         return array
     except Exception as e:
         print("couldn't find a element with a tag. returning empty array")
-        print("An error occurred at finding element with tags:", e)
-        return []
-    
+        raise ValueError("couldn't find a element. Relaunch the exe")
+def choose_save_location():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main tkinter window
+
+    file_path = filedialog.asksaveasfilename(
+        defaultextension=".png",
+        filetypes=[("PNG Files", "*.png"), ("All Files", "*.*")],
+        title="Save Screenshot As"
+    )
+
+    return file_path
 def export_to_json(data, file_name):
     try:
         with open(file_name, 'w') as file:
@@ -68,16 +87,25 @@ soup = BeautifulSoup(page_source, "html.parser")
 # # Find the first element with the specified class
 single_element = soup.find(class_='css-1p2cbqg')
 
+links=[]
 games_name=[]
 games_date=[]
 images=[]
+links=find_element_tag(single_element,'a','href')
+full_links=['https://store.epicgames.com'+element for element in links]
 
 games_name=find_element(single_element,'css-1h2ruwl')
 games_date=find_element(single_element,'css-nf3v9d')
 images=find_element_tag(single_element,'img','data-image')
-print(games_name)
-print(games_date)
-print(images)
+print("info!!!")
+print("**********************************")
+print("**********************************")
+print("{{Links}}",full_links)
+print("{{Name}}",games_name)
+print("{{Date}}",games_date)
+print("{{Image Links}}",images)
+print("**********************************")
+print("**********************************")
 
 
 driver.quit()
@@ -90,5 +118,84 @@ combined_data = {
 }
 
 # Call the function to export the combined data as JSON
-export_to_json(combined_data, 'export/output.json')
+export_to_json(combined_data, '_export/output.json')
 
+#at line 112 is not very effective to write like that but it works
+#TODO Make a better way of writing line 112
+html_content = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div class="container">
+        <div class="logo">
+            <img src="logo.png" alt="epic">
+            <h2>Free Gaming</h2>
+        </div>
+    <div class="flex-container">
+        <!-- Loop through the games data -->
+        {"".join(f'''
+        <div class="game-info">
+            <div class="head"><p>{game_name}</p></div>
+            
+            <div class="images">
+                <img src="{image_url}" alt="Game Image">
+                {'<div class="alert_p"><p>Free Now</p></div>' if 'Free Now' in game_date else '<div class="alert_p coming_soon"><p>Coming Soon</p></div>'}
+            </div>
+            <p>{game_date.replace("Free ", 'Free At: ').replace("Free At: Now", '<span class="highlight">Free Now Until</span>')}</p>
+        </div>
+        ''' for game_name, game_date, image_url in zip(combined_data['games_name'], combined_data['games_date'], combined_data['image_urls']))}
+    </div>
+    </div>
+</body>
+</html>
+"""
+
+try:
+    with open("_web/index.html", "w") as html_file:
+        html_file.write(html_content)
+    print("\nHTML file created successfully!")
+except Exception as e:
+    print("\nAn error occurred:", str(e))
+    print("Failed to create HTML file.")
+
+# Use Selenium to capture a screenshot of the rendered HTML content
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('file://' + os.path.abspath("_web/index.html"))  # Load the local HTML file
+
+total_height_script = "return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );"
+total_content_height = driver.execute_script(total_height_script)
+
+total_width_script = "return Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );"
+total_content_width = driver.execute_script(total_width_script)
+
+# Set the browser window size to the desired resolution (width x height)
+desired_width = total_content_width-5
+desired_height = total_content_height-5  # Add some buffer for potential scrollbars
+driver.set_window_size(desired_width, desired_height)
+
+screenshot = driver.get_screenshot_as_png()
+
+driver.quit()
+
+# Convert the screenshot to an Image object using Pillow
+screenshot_image = Image.open(BytesIO(screenshot))
+
+# Ask the user for the save location
+file_path = filedialog.asksaveasfilename(
+        defaultextension=".png",
+        initialfile="freeEpicGames.png",  # Default name
+        filetypes=[("PNG Files", "*.png"), ("All Files", "*.*")],
+        title="Save PNG As"
+    )
+
+if file_path:
+    screenshot_image.save(file_path,dpi=(300, 300))
+    print(f"Screenshot saved as {file_path}")
+else:
+    print("Screenshot not saved.")
+
+# Display a prompt and wait for user input
+input("Script finished. Press Enter to close...")
